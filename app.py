104f756ef9a32775a402dd1f2ff96737819a5f64
BONIFACE EMMANUEL CHOSEN <65294466+bechosen-spec@users.noreply.github.com>
2025-04-14 16:17:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ except sqlite3.Error as e:
 # --------------------------
 @st.cache_resource
 def load_artifacts():
-    model = joblib.load('models/best_xgb_model (4).pkl')
+    model = joblib.load('models/fraud_detection_model.pkl')
     scaler = joblib.load('models/scaler.pkl')
     diagnosis_encoder = joblib.load('models/diagnosis_encoder.pkl')
     fraud_encoder = joblib.load('models/fraud_encoder.pkl')
